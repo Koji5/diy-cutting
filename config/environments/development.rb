b494b9faa1b91616ff1ef60254aa4793f96d963e
Koji5 <sakamoto5002@gmail.com>
2025-05-11 13:50:35 +0900

    Redis 抜きの最小構成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,10 @@ Rails.application.configure do
   end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # 後から切り替えられるようにコメントアウト
+  # config.cache_store = :memory_store
+  config.cache_store = :null_store
+  config.action_cable.mount_path = nil   # or disable request forgery
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
