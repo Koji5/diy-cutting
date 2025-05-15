174080bfbb5ca817d8a0f5573aaa005be92bdb0f
Koji5 <sakamoto5002@gmail.com>
2025-05-15 13:01:07 +0900

    TOPページ表示（仮）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "home/index"
+  get "articles/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root to: "home#index"            # 仮置き
+  root "articles#index"            # トップページを articles#index に
+  resources :articles, only: [:index]
   devise_for :users
 end
